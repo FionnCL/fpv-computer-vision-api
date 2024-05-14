c74138712cbad24ead988e612ae388d8d98e4afe
FionnCL <camacholenihanfionn@gmail.com>
2024-05-14 18:06:58 +0100

    Added dependencies.

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-use std::io::Cursor;
+use std::io::Cursor as Cursor;
 use image::io::Reader as ImageReader;
 
 #[macro_use] extern crate rocket;
@@ -9,10 +9,16 @@ fn index(image: Vec<u8>)-> &'static Vec<u8> {
     //  - and do stuff!
     
     // do computer vision
-    let bounding_box: Vec<u8> = computerVision();
+    let bounding_box: Vec<u8> = computer_vision();
     
     // get centre of person and return
-    vec![((bounding_box[x1] + bounding_box[x2]) / 2), ((bounding_box[y1] + bounding_box[y2]) / 2)]
+    let x1 = 1; let x2 = 2;
+    let y1 = 3; let y2 = 4;
+    vec![round_down(x1 + x2 / 2), round_down(y1 + y2 / 2)]
+}
+
+fn computer_vision() -> Vec<u8> {
+    vec![1, 2, 3]
 }
 
 #[launch]
