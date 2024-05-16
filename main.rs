3749ed6a38fddc48321de293bef588bd9c360965
FionnCL <camacholenihanfionn@gmail.com>
2024-05-16 16:31:50 +0100

    Learning the damn language.

--- main.rs
+++ main.rs
@@ -1,24 +1,39 @@
 use std::io::Cursor as Cursor;
 use image::io::Reader as ImageReader;
+use rocket::form::Form;
+
+#[derive(FromForm)]
+struct Task {
+    complete: bool,
+    description: String
+}
 
 #[macro_use] extern crate rocket;
 
-#[post("/")]
-fn index(image: Vec<u8>)-> &'static Vec<u8> {
-    // get image
-    //  - and do stuff!
-    
+#[post("/", data = "<task>")]
+fn index(task: Form<Task>)-> &'static str {
     // do computer vision
-    let bounding_box: Vec<u8> = computer_vision();
+    let bounding_box:Vec<u8> = computerVision();
     
+    // Get the necessary instructions
+    let instructions: str = getInstructions(&bounding_box);
+
     // get centre of person and return
-    let x1 = 1; let x2 = 2;
-    let y1 = 3; let y2 = 4;
-    vec![round_down(x1 + x2 / 2), round_down(y1 + y2 / 2)]
+    &instructions
 }
 
-fn computer_vision() -> Vec<u8> {
+fn computerVision() -> Vec<u8> {
     vec![1, 2, 3]
+    // realistically what will happen here is one of two things:
+    //  - use a locally trained CV model
+    //  - make request for more instructions from remote CV model API.
+    // either way what shall be returned here is a bounding box-
+    // that returns the "coordinates" to the person that is nearest to-
+    // the centre of the screen.
+}
+
+fn getInstructions() -> &'static str {
+    "Test"
 }
 
 #[launch]
