a2f6edb67c13f41bf0cc539ae23fd0348289ed16
FionnCL <camacholenihanfionn@gmail.com>
2024-05-19 11:09:16 +0100

    Attempting to allow images to be sent in the post request body.

--- main.rs
+++ main.rs
@@ -1,31 +1,26 @@
-//use rocket::form::{Form, DataField, FromFormField};
+use rocket::form::{Form, DataField, FromFormField};
 //use rocket::http::{ContentType, CookieJar, Status};
 
-use std::{
-    env,
-    error::Error,
-    io,
-    ffi::OsString,
-    fs::File,
-    process
-};
+use std::fs::File;
 
 #[macro_use] extern crate rocket;
 
-#[post("/")]
-fn index(task: File)-> &'static str{
-    // do computer vision
-    let bounding_box:Vec<u8> = computer_vision();
+struct Image {
+    image: File,
+    name: str
+}
+
+#[post("/", format = "multipart/form-data", data = "<image>")]
+fn index(image: 'File) -> String {
+    // perform computer vision
+    let bounding_box:Vec<f32> = get_bounding_box(image);
     
     // Get the necessary instructions
     // get_instructions(bounding_box)
-    (std::fs::read_to_string(task)).as_str()
+    format!("{},{}", (0.5 - &bounding_box[0]), (0.5 - &bounding_box[1]))
 }
 
-fn computer_vision(coordinates: File) -> Vec<f32> {
-    let mut deconstructed_values = run().iter().position(|x| * x == 0).unwrap(); 
-
-    vec![0.451512, 0.124531, 0.314123, 0.532123]
+fn get_bounding_box(coordinates: Image) -> Vec<f32> {
     // realistically what will happen here is one of two things:
     //  - use a locally trained CV model
     //  - make request for more instructions from remote CV model API.
@@ -36,33 +31,13 @@ fn computer_vision(coordinates: File) -> Vec<f32> {
     // <class_id> <x_centre> <y_centre> <width> <height>
     // We can forget about class_id as we will only be dealing with one:
     //  - person
+    // going to assume we have obtained the values into a vector:
+    vec![0.451512, 0.124531, 0.314123, 0.532123]
 }
 
-fn get_instructions(bounding_box: Vec<f32>) -> &'static str {
-    "Test"
-}
-
-fn run() -> Result<(), Box<dyn Error>> {
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::new()
-        .has_headers(false)
-        .delimiter(b' ')
-        .from_reader(file);
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
-
-/// Returns the first positional argument sent to this process. If there are no
-/// positional arguments, then this returns an error.
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
+fn get_instructions(bounding_box: Vec<f32>) -> () {
+    // Return value should be 2 floats denoting how much to move
+    // in the x and y directions.
 }
 
 #[launch]
