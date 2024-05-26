2eeccdecd1cb8870b238ad42806e9908fa1e430c
FionnCL <camacholenihanfionn@gmail.com>
2024-05-26 18:54:36 +0100

    Attempting to fix the problem of 0 learning.

--- trainer.py
+++ trainer.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-from tqdm.notebook import tqdm
-
 
 class CombatantDataset(Dataset):
     def __init__(self, data_dir, transform=None):
@@ -35,13 +33,20 @@ class SimpleCombatantClassifier(nn.Module):
         self.features = nn.Sequential(*list(self.base_model.children())[:-1])
 
         enet_out_size = 1280
-        self.classifier = nn.Linear(enet_out_size, num_classes)
+        self.classifier = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(enet_out_size, num_classes)
+        )
 
     def forward(self, x):
         x = self.features(x)
         output = self.classifier(x)
         return output
 
+
+# Initialize model, datasets, and training parameters.
+learning_rate = 0.1
+batch_size = 4
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -50,16 +55,20 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-data_dir = './train'
+train_dir = './train'
+valid_dir = './valid'
 
 model = SimpleCombatantClassifier(num_classes=1)
 model.to(device)
-dataset = CombatantDataset(data_dir, transform)
 
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+train_dataset = CombatantDataset(train_dir, transform=transform)
+valid_dataset = CombatantDataset(valid_dir, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=.001)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 num_epochs = 5
 train_losses, val_losses = [], []
@@ -67,28 +76,27 @@ train_losses, val_losses = [], []
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
-    for images, labels in dataloader:
+    for images, labels in train_loader:
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        running_loss += loss.item() * images.size(0)
-    train_loss = running_loss / len(dataloader.dataset)
+        running_loss += loss.item() * labels.size(0)
+    train_loss = running_loss / len(train_loader.dataset)
     train_losses.append(train_loss)
 
     model.eval()
     running_loss = 0.0
-
     with torch.no_grad():
-        for images, labels in dataloader:
+        for images, labels in valid_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criterion(outputs, labels)
-            running_loss += loss.item() * images.size(0)
-        val_loss = running_loss / len(dataloader.dataset)
-        val_losses.append(val_loss)
+            running_loss += loss.item() * labels.size(0)
+    val_loss = running_loss / len(valid_loader.dataset)
+    val_losses.append(val_loss)
 
     print(f"Epoch {epoch+1}/{num_epochs} - Train loss: {train_loss}," +
           f"Validation loss: {val_loss}")
