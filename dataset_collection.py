2941bc8e9bbcac3dd37c229f3eb4776614da7f84
FionnCL <camacholenihanfionn@gmail.com>
2024-05-26 12:39:53 +0100

    Starting object detection over.

--- dataset_collection.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-
-dataset = tfds.load('horses_or_humans', split='train')
-for datum in dataset:
-    image, bboxes = datum["image"], datum["bboxes"]
